5598806f457a30a5ed2455947a998b1b765ca6f3
Goodness <goodnessabisoye@gmail.com>
2024-03-04 03:52:51 +0100

    sleep

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   };
   const [submittedExperienceValue, setSubmittedExperienceValue] = useState(null)
   const handleExperienceFormSubmit = (value) => {
-    setExperienceSubmittedValue(value);
+    setSubmittedExperienceValue(value);
     alert(submittedExperienceValue)
   };
 
